e414aaaed5c2337d0a232236c4f50fdda301b245
zejuzhaodaheilong <zejuzhao20@gmail.com>
2026-05-22 11:51:23 +0800

    Implement binary search algorithm in C

---
+++ Binary Search.c
@@ -0,0 +1,20 @@
+int binarySearch(int arr[], int n, int target) {
+   int left = 0;
+   int right = n - 1;
+   
+   while (left <= right) {
+      int mid = left + (right - left) / 2;
+      
+      if (arr[mid] == target) {
+         return mid;
+      } 
+   else if (arr[mid] < target) {
+         left = mid + 1;   // 丢左边
+      } 
+   else {
+         right = mid - 1;  // 丢右边
+      }
+   }
+   
+   return -1;
+} 
